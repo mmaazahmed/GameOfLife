0c7e426290678c55175d7e551def1fc5bc3a3a47
mmaazahmed <maazahmedpoke@gmail.com>
2023-12-05 21:18:33 +1100

    addded pause and mouse interactivity

---
+++ grid.js
@@ -0,0 +1,11 @@
+const canvas=document.getElementById('mycanvas');
+canvas.style.background="#1729";
+const ctx=canvas.getContext("2d");
+canvas.height=window.innerHeight;
+canvas.width=window.innerWidth;
+
+const length=5; //px
+
+function draw_board(length){
+
+}
