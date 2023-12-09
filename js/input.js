24e687dd71e059fe6013709e7302f2785a343471
mmaazahmed <maazahmedpoke@gmail.com>
2023-12-10 05:27:53 +1100

    add/remove Cell bug fix

--- js/input.js
+++ js/input.js
@@ -8,23 +8,25 @@
  */
 const Scroll = {
   _scrollIncrement: 5,
-  up: function (game, event) {
+  up(game, event) {
     if (event.key !== 'ArrowUp') { return; }
     this.scrollCells(game, 0, this._scrollIncrement);
+    console.log(game.displayDx,game.displayDy);
+
   },
-  down: function (game, event) {
+  down(game, event) {
     if (event.key !== 'ArrowDown') { return; }
     this.scrollCells(game, 0, -this._scrollIncrement);
   },
-  left: function (game, event) {
+  left(game, event) {
     if (event.key !== 'ArrowLeft') { return; }
     this.scrollCells(game,this._scrollIncrement, 0);
   },
-  right: function (game, event) {
+  right(game, event) {
     if (event.key !== 'ArrowRight') { return; }
     this.scrollCells(game, -this._scrollIncrement, 0);
   },
-  scrollCells: function (game, dx, dy) {
+  scrollCells (game, dx, dy) {
     game.displayDx+=dx;
     game.displayDy+=dy;
   }
@@ -32,28 +34,28 @@ const Scroll = {
 
 const Zoom={
     _oldMousePos:'',
-    getZoomFactor: function(game,deltaY){
+    getZoomFactor(game,deltaY){
       let zoomFactor=Math.min(5,Math.abs(deltaY))/20;
       zoomFactor= deltaY<0 ? 1-zoomFactor : zoomFactor+1;
       return zoomFactor
     },
-    in: function(game, event) {
+    in(game, event) {
         if (event.keyCode !== 107) { return; }
         game.cellSize =Math.min(200,game.cellSize*this.getZoomFactor(game,5));
       },
 
-    out:function(game, event) {
+    out(game, event) {
         if (event.keyCode !== 109) { return; }
         game.cellSize = Math.max(1, game.cellSize*this.getZoomFactor(game,-5));
       },
-    gestureZoom:function(ctx,game,event){
+    gestureZoom(ctx,game,event){
         this.oldMousePos =getMousePosOnBoard(ctx,game,event);
         let deltaY=Math.floor(event.deltaY);
         const zoomFactor=this.getZoomFactor(game,deltaY);
         game.cellSize=Math.min(50,Math.max(1,game.cellSize*zoomFactor));
         this.centerOnZoom(ctx,game,event);
       },
-    centerOnZoom:function(ctx,game,event){
+    centerOnZoom(ctx,game,event){
       const [oldmouseX,oldmouseY] =this.oldMousePos.split(",").map(Number);
       const [newMouseX,newMouseY] = getMousePosOnBoard(ctx,game,event).split(",").map(Number);
       Scroll.scrollCells(game,newMouseX-oldmouseX,0);
@@ -62,27 +64,28 @@ const Zoom={
     }
 }
 const Speed={
-    up: function(game, event) {
+    up(game, event) {
         if (event.keyCode !== 104) {return;}
         game.updateInterval= Math.max(0, game.updateInterval-10);
       },
-    down: function(game, event) {
+    down(game, event) {
         if (event.keyCode !== 105) {return;}
         game.updateInterval = Math.min(500, game.updateInterval+10);
       }
 }
 const Cell={
-    add: function(game,clickedCell){
+    add(game,clickedCell){
+        console.log(clickedCell);
         game.activeCells.add(clickedCell);
     },
-    remove:function(game,clickedCell){
+    remove(game,clickedCell){
         game.activeCells.delete(clickedCell);
     },
-    changeBackground: function(ctx, event) {
+    changeBackground(ctx, event) {
       // const canvas=ctx.canvas;
         
       },
-    changeColor:function (canvas, event) {
+    changeColor(canvas, event) {
         //todo
       }
     
@@ -99,12 +102,13 @@ function getMousePosOnBoard(ctx, game, event) {
   const canvasRect = ctx.canvas.getBoundingClientRect();
 
   const { clientX, clientY } = event;
+  
   const { left: canvasX, top: canvasY } = canvasRect;
 
   const { relativeX, relativeY } = { relativeX: clientX - canvasX, relativeY: clientY - canvasY };
   const { gridX, gridY } = { gridX: Math.floor(relativeX / cellSize), gridY: Math.floor(relativeY / cellSize) };
 
-  return `${gridX},${gridY}`;
+  return `${gridX-game.displayDx},${gridY-game.displayDy}`;
 }
 
 
